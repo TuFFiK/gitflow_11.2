ca529f04aecf6da087f0aaa285149fab4fc1636b
Vladimir Strazev <vladimir.strazev@gmail.com>
2024-01-26 03:29:00 +0000

    Add capitalize_first_letters function with docstring

--- str_func.py
+++ str_func.py
@@ -3,3 +3,14 @@ def capitalize_words(sentence):
     words = sentence.split()
     capitalized_words = [word.capitalize() for word in words]
     return ' '.join(capitalized_words)
+
+def capitalize_first_letters(sentence):
+    """Capitalize the first letter of each word in a sentence.
+
+    Args:
+        sentence (str): The input sentence.
+
+    Returns:
+        str: The sentence with the first letter of each word capitalized.
+    """
+    return ' '.join([word.capitalize() for word in sentence.split()])
